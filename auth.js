f6c0597eaa5f7b453c3658003b19023d270d2ef6
kunaltiwari4343-cell <kunaltiwari4343@gmail.com>
2026-08-16 00:36:25 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import { db } from './db.js';
-// import { signToken, authMiddleware } from '../auth.js';
+import { signToken, authMiddleware } from '../auth.js';
 
 const router = express.Router();
 
